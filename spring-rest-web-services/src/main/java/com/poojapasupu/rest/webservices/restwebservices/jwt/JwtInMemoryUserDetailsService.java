f6a624769815f5389dba44e510b7172089f4cf77
poojapasupu <pooja.paspu@gmail.com>
2021-08-30 20:12:38 -0500

    modifying users

--- spring-rest-web-services/src/main/java/com/poojapasupu/rest/webservices/restwebservices/jwt/JwtInMemoryUserDetailsService.java
+++ spring-rest-web-services/src/main/java/com/poojapasupu/rest/webservices/restwebservices/jwt/JwtInMemoryUserDetailsService.java
@@ -15,11 +15,8 @@ public class JwtInMemoryUserDetailsService implements UserDetailsService {
   static List<JwtUserDetails> inMemoryUserList = new ArrayList<>();
 
   static {
-    inMemoryUserList.add(new JwtUserDetails(1L, "in28minutes",
-        "$2a$10$3zHzb.Npv1hfZbLEU5qsdOju/tk2je6W6PnNnY.c1ujWPcZh4PL6e", "ROLE_USER_2"));
     inMemoryUserList.add(new JwtUserDetails(2L, "pooja",
             "$2a$10$5cbFhqSsL/2bKh4aIk9.HeiMpFy6Sk6GGF.fAw5C4.gxB4Q914GSK", "ROLE_USER_2"));
-
 
   }
 
